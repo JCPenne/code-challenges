2f119c87934e3adc742a83db13488d9f4a57f85a
JCPenne <jackcpalmer@icloud.com>
2023-05-25 18:58:37 +0000

    Sync LeetCode submission - Shuffle the Array (python)

---
+++ problems/shuffle_the_array/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        RESULT = []
+        i = 0
+
+        while i < n:
+            RESULT.extend((nums[i], nums[i + n]))
+            i += 1
+
+        return RESULT
