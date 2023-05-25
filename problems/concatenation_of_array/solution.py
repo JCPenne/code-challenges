f285582a5d6a99161604e68a6b353db8e56931a9
JCPenne <jackcpalmer@icloud.com>
2023-05-25 18:41:01 +0000

    Sync LeetCode submission - Concatenation of Array (python)

---
+++ problems/concatenation_of_array/solution.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        LENGTH = len(nums)
+        for num in range(LENGTH):
+            nums.append(nums[num])
+        return nums
